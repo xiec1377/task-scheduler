35aff4301cd6ceeb8d548b33b9c68f0ff10bd625
Catherine Xie <catherinexie2014@gmail.com>
2024-12-13 01:25:11 -0500

    style: layout for cards

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,11 @@
 // import Image from "next/image";
 import TaskForm from "@/components/TaskForm";
 
+// grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="">
+      <main className="flex flex-col items-center p-20">
         <TaskForm />
       </main>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
--- frontend/components/TaskForm.tsx
+++ frontend/components/TaskForm.tsx
@@ -78,131 +78,127 @@ export default function TaskForm() {
     }
   };
   console.log("schedule:", schedule);
-  // const myEventsList = schedule;
-  // const myEventsList = [
-  //   {
-  //     title: "Meeting with Team",
-  //     start: new Date(2024, 11, 12, 10, 0), // Dec 12, 2024, 10:00 AM
-  //     end: new Date(2024, 11, 12, 11, 0), // Dec 12, 2024, 11:00 AM
-  //   },
-  //   {
-  //     title: "Project Deadline",
-  //     start: new Date(2024, 11, 15, 9, 0), // Dec 15, 2024, 9:00 AM
-  //     end: new Date(2024, 11, 15, 17, 0), // Dec 15, 2024, 5:00 PM
-  //   },
-  // ];
 
   const localizer = momentLocalizer(moment);
 
   return (
-    <div className="w-[80vh] p-6 flex flex-col items-center gap-4">
-      <h1 className="text-3xl font-bold text-white mb-6">Task Scheduler</h1>
-      <div className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full max-w-lg">
-        <div className="space-y-4">
-          <div>
-            <label
-              htmlFor="priority"
-              className="block text-sm font-medium text-white mb-1"
-            >
-              Task Name
-            </label>
-            <input
-              type="text"
-              // placeholder="Task Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full border bg-transparent border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+    <div className="w-[90vw] p-6 flex flex-col items-center gap-4">
+      <h1 className="w-full text-3xl font-bold text-center text-white mb-6">
+        Task Scheduler
+      </h1>
+      <div className="w-full flex flex-row items-start justify-center gap-4">
+        <div className="w-1/3 flex flex-col items-start gap-4">
+          <div className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full">
+            <h2 className="text-xl font-semibold text-white mb-4">
+              Create a new task
+            </h2>
+            <div className="space-y-4">
+              <div>
+                <label
+                  htmlFor="priority"
+                  className="block text-sm font-medium text-white mb-1"
+                >
+                  Task Name
+                </label>
+                <input
+                  type="text"
+                  // placeholder="Task Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full border bg-transparent border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="priority"
+                  className="block text-sm font-medium text-white mb-1"
+                >
+                  Priority (1-10)
+                </label>
+                <input
+                  type="number"
+                  // placeholder="Difficulty (1-10)"
+                  min="1"
+                  max="10"
+                  value={difficulty}
+                  onChange={(e) => setDifficulty(e.target.value)}
+                  className="w-full border bg-transparent border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="priority"
+                  className="block text-sm font-medium text-white mb-1"
+                >
+                  Difficulty (1-10)
+                </label>
+                <input
+                  type="number"
+                  // placeholder="Priority (1-10)"
+                  min="1"
+                  max="10"
+                  value={priority}
+                  onChange={(e) => setPriority(e.target.value)}
+                  className="w-full border bg-transparent border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="time"
+                  className="block text-sm font-medium text-white mb-1"
+                >
+                  Time (min)
+                </label>
+                <input
+                  id="time"
+                  type="number"
+                  // placeholder="Time (min)"
+                  min="1"
+                  // value={time}
+                  onChange={(e) => setTime(e.target.value)}
+                  className="w-full border bg-transparent  border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <button
+                onClick={addTask}
+                className="w-full bg-blue-500 text-white rounded-lg p-2 hover:bg-blue-600 transition"
+              >
+                Add Task
+              </button>
+            </div>
           </div>
-          <div>
-            <label
-              htmlFor="priority"
-              className="block text-sm font-medium text-white mb-1"
+          <div className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full">
+            <h2 className="text-xl font-semibold text-white mb-4">Tasks</h2>
+            <ul className="space-y-2">
+              {tasks.map((task, index) => (
+                <li key={index} className="rounded-lg p-3 shadow-sm">
+                  <span className="font-medium">{task.name}</span> (Difficulty:{" "}
+                  {task.difficulty}, Priority: {task.priority}, Time:{" "}
+                  {task.time})
+                </li>
+              ))}
+            </ul>
+            <button
+              onClick={generateSchedule}
+              className="mt-4 w-full bg-green-500 text-white rounded-lg p-2 hover:bg-green-600 transition"
             >
-              Priority (1-10)
-            </label>
-            <input
-              type="number"
-              // placeholder="Difficulty (1-10)"
-              min="1"
-              max="10"
-              value={difficulty}
-              onChange={(e) => setDifficulty(e.target.value)}
-              className="w-full border bg-transparent border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+              Generate Schedule
+            </button>
           </div>
-          <div>
-            <label
-              htmlFor="priority"
-              className="block text-sm font-medium text-white mb-1"
-            >
-              Difficulty (1-10)
-            </label>
-            <input
-              type="number"
-              // placeholder="Priority (1-10)"
-              min="1"
-              max="10"
-              value={priority}
-              onChange={(e) => setPriority(e.target.value)}
-              className="w-full border bg-transparent border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="time"
-              className="block text-sm font-medium text-white mb-1"
-            >
-              Time (min)
-            </label>
-            <input
-              id="time"
-              type="number"
-              // placeholder="Time (min)"
-              min="1"
-              // value={time}
-              onChange={(e) => setTime(e.target.value)}
-              className="w-full border bg-transparent  border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <button
-            onClick={addTask}
-            className="w-full bg-blue-500 text-white rounded-lg p-2 hover:bg-blue-600 transition"
-          >
-            Add Task
-          </button>
         </div>
-      </div>
-      <Calendar
-        localizer={localizer}
-        events={schedule ?? []}
-        selectable={true} 
-        startAccessor="start"
-        endAccessor="end"
-        view="day"
-        style={{ height: 500 }}
-        className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full max-w-lg text-white"
-      />
-      <div className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full max-w-lg">
-        <h2 className="text-xl font-semibold text-white mb-4">Tasks</h2>
-        <ul className="space-y-2">
-          {tasks.map((task, index) => (
-            <li key={index} className="rounded-lg p-3 shadow-sm">
-              <span className="font-medium">{task.name}</span> (Difficulty:{" "}
-              {task.difficulty}, Priority: {task.priority}, Time: {task.time})
-            </li>
-          ))}
-        </ul>
-        <button
-          onClick={generateSchedule}
-          className="mt-4 w-full bg-green-500 text-white rounded-lg p-2 hover:bg-green-600 transition"
-        >
-          Generate Schedule
-        </button>
+        <Calendar
+          localizer={localizer}
+          events={schedule ?? []}
+          selectable={true}
+          startAccessor="start"
+          endAccessor="end"
+          view="day"
+          className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-2/3 h-[100vh] text-white"
+        />
       </div>
 
-      {schedule && (
-        <div className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full max-w-lg">
+      {/* {schedule && (
+        <div className="bg-white/20 backdrop-blur-md border border-white/20 shadow-lg rounded-lg p-8 shadow-md rounded-lg p-6 w-full">
           <h2 className="text-xl font-semibold text-white mb-4">Schedule</h2>
           <ul className="space-y-2">
             {schedule?.map((item, index) => (
@@ -213,7 +209,7 @@ export default function TaskForm() {
             ))}
           </ul>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
